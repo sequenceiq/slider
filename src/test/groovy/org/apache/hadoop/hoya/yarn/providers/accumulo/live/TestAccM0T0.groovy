349a34a4b2815c2133f76e99878973722523dea7
Steve Loughran <stevel@hortonworks.com>
2013-09-11 09:39:47 +0100

    working test to run a masterless Accumulo cluster

---
+++ src/test/groovy/org/apache/hadoop/hoya/yarn/providers/accumulo/live/TestAccM0T0.groovy
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.hadoop.hoya.yarn.providers.accumulo.live
+
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import org.apache.hadoop.hoya.api.ClusterDescription
+import org.apache.hadoop.hoya.providers.accumulo.AccumuloKeys
+import org.apache.hadoop.hoya.yarn.ZKIntegration
+import org.apache.hadoop.hoya.yarn.client.HoyaClient
+import org.apache.hadoop.hoya.yarn.providers.accumulo.AccumuloTestBase
+import org.apache.hadoop.yarn.service.launcher.ServiceLauncher
+import org.junit.Test
+
+@CompileStatic
+@Slf4j
+class TestAccM0T0 extends AccumuloTestBase {
+
+  @Test
+  public void testAccM0T0() throws Throwable {
+    String clustername = "TestAccM0T0"
+    createMiniCluster(clustername, createConfiguration(), 1, 1, 1, true, true)
+    describe(" Create an accumulo cluster");
+
+    //make sure that ZK is up and running at the binding string
+    ZKIntegration zki = createZKIntegrationInstance(ZKBinding, clustername, false, false, 5000)
+    log.info("ZK up at $zki");
+    //now launch the cluster
+    ServiceLauncher launcher = createAccCluster(clustername, 
+                                                [(AccumuloKeys.ROLE_MASTER): 0],
+                                                [], true, true)
+    HoyaClient hoyaClient = (HoyaClient) launcher.service
+    addToTeardown(hoyaClient);
+    ClusterDescription status = hoyaClient.getClusterStatus(clustername)
+    describe("Cluster status")
+    log.info(prettyPrint(status.toJsonString()))
+
+  }
+
+}
