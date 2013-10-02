e8045b02342299a7b76e47bc251aa35a7a1d1420
Steve Loughran <stevel@hortonworks.com>
2013-10-02 10:51:19 +0100

    script work: looking at relaying to hadop JAR

--- src/scripts/hoya.sh
+++ src/scripts/hoya.sh
@@ -16,4 +16,34 @@
 # limitations under the License.
 
 
-java -jar target/hoya-0.3-SNAPSHOT.jar org.apache.hadoop.hoya.yarn.client.HoyaClient "$@"
+# this is the shell script to start Hoya deploying an application
+# Usage: hoya [--config confdir] <action> <commands>
+
+function usage
+{
+  echo "Usage: hoya [--config confdir]  <action> <arguments>"
+  echo ""
+  echo "Most commands print help when invoked w/o parameters."
+}
+
+# Hoya works out its own location 
+this="${BASH_SOURCE-$0}"
+bin=$(cd -P -- "$(dirname -- "$this")" && pwd -P)
+script="$(basename -- "$this")"
+this="$bin/$script"
+
+# lib directory is one up; it is expected to contain 
+# hoya.jar and any other dependencies that are not in the
+# standard Hadoop classpath
+
+libdir="${bin}/"
+
+# plan
+# if: --confdir is set, set env dir HADOOP_CONF_DIR
+
+# HADOOP_PREFIX is then basedir 
+
+export HADOOP_CLASSPATH="${libdir}:\*.jar"
+launcher=org.apache.hadoop.yarn.service.launcher.ServiceLauncher
+
+hadoop ${launcher} org.apache.hadoop.hoya.Hoya
