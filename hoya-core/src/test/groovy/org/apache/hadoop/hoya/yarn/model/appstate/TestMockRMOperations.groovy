24128c18828b93d197af1685380b842100821bfb
Josh Elser <josh.elser@gmail.com>
2013-12-09 22:12:08 -0500

    BUG-11304 Remove the unnecessary import.

--- hoya-core/src/test/groovy/org/apache/hadoop/hoya/yarn/model/appstate/TestMockRMOperations.groovy
+++ hoya-core/src/test/groovy/org/apache/hadoop/hoya/yarn/model/appstate/TestMockRMOperations.groovy
@@ -18,7 +18,6 @@
 
 package org.apache.hadoop.hoya.yarn.model.appstate
 
-import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import org.apache.hadoop.hoya.exceptions.HoyaRuntimeException
 import org.apache.hadoop.hoya.yarn.appmaster.state.AbstractRMOperation
