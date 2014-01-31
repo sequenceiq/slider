e5129e88d097fa930c63db3dd52e202dbe111502
Steve Loughran <stevel@hortonworks.com>
2014-01-31 12:36:11 +0000

    heap size of yarn services in -secure cluster

--- hoya-funtest/src/test/configs/ubuntu-secure/yarn-env.sh
+++ hoya-funtest/src/test/configs/ubuntu-secure/yarn-env.sh
@@ -36,7 +36,7 @@ JAVA_HEAP_MAX=-Xmx256m
 
 # For setting YARN specific HEAP sizes please use this
 # Parameter and set appropriately
-# YARN_HEAPSIZE=1000
+YARN_HEAPSIZE=256
 
 # check envvars which might override default args
 if [ "$YARN_HEAPSIZE" != "" ]; then
