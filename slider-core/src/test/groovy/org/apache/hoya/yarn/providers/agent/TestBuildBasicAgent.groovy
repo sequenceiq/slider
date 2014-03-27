35128668c966770f8c3d7b775b552f053230d35e
Steve Loughran <stevel@hortonworks.com>
2014-03-27 18:23:57 +0000

    BUG-15582 update TestBuildBasicAgent args before next tests go in 

--- slider-core/src/test/groovy/org/apache/hoya/yarn/providers/agent/TestBuildBasicAgent.groovy
+++ slider-core/src/test/groovy/org/apache/hoya/yarn/providers/agent/TestBuildBasicAgent.groovy
@@ -25,7 +25,6 @@ import org.apache.hadoop.yarn.service.launcher.ServiceLauncher
 import org.apache.hoya.api.ResourceKeys
 import org.apache.hoya.exceptions.BadConfigException
 import org.apache.hoya.providers.agent.AgentKeys
-import org.apache.hoya.yarn.Arguments
 import org.apache.hoya.yarn.client.HoyaClient
 import org.junit.Test
 
@@ -43,56 +42,57 @@ class TestBuildBasicAgent extends AgentTestBase {
   }
 
   @Test
-    public void testBuildMultipleRoles() throws Throwable {
+  public void testBuildMultipleRoles() throws Throwable {
 
-        def clustername = "test_build_basic_agent"
-        createMiniCluster(
-                clustername,
-                configuration,
-                1,
-                1,
-                1,
-                true,
-                false)
-        buildAgentCluster("test_build_basic_agent_node_only",
-                [(AgentKeys.ROLE_NODE): 5],
-                [
-                        ARG_OPTION, CONTROLLER_URL, "http://localhost",
-                        ARG_PACKAGE, ".",
-                        ARG_OPTION, SCRIPT_PATH, "agent/scripts/agent.py",
-                        ARG_COMP_OPT, AgentKeys.ROLE_NODE, SCRIPT_PATH, "agent/scripts/agent.py",
-                        ARG_RES_COMP_OPT, AgentKeys.ROLE_NODE, ResourceKeys.COMPONENT_PRIORITY, "1",
-                ],
-                true, false,
-                false)
+    def clustername = "test_build_basic_agent"
+    createMiniCluster(
+        clustername,
+        configuration,
+        1,
+        1,
+        1,
+        true,
+        false)
+    buildAgentCluster("test_build_basic_agent_node_only",
+        [(AgentKeys.ROLE_NODE): 5],
+        [
+            ARG_OPTION, CONTROLLER_URL, "http://localhost",
+            ARG_PACKAGE, ".",
+            ARG_OPTION, SCRIPT_PATH, "agent/scripts/agent.py",
+            ARG_COMP_OPT, AgentKeys.ROLE_NODE, SCRIPT_PATH, "agent/scripts/agent.py",
+            ARG_RES_COMP_OPT, AgentKeys.ROLE_NODE, ResourceKeys.COMPONENT_PRIORITY, "1",
+        ],
+        true, false,
+        false)
 
-        def master = "hbase-master"
-        def rs = "hbase-rs"
-        ServiceLauncher<HoyaClient> launcher = buildAgentCluster(clustername,
-                [
-                        (AgentKeys.ROLE_NODE): 5,
-                        (master): 1,
-                        (rs): 5
-                ],
-                [
-                        ARG_OPTION, CONTROLLER_URL, "http://localhost",
-                        ARG_OPTION, PACKAGE_PATH, ".",
-                        ARG_COMP_OPT, master, SCRIPT_PATH, "agent/scripts/agent.py",
-                        ARG_COMP_OPT, rs, SCRIPT_PATH, "agent/scripts/agent.py",
-                        ARG_RES_COMP_OPT, master, ResourceKeys.COMPONENT_PRIORITY, "2",
-                        ARG_RES_COMP_OPT, rs, ResourceKeys.COMPONENT_PRIORITY, "3",
-                        ARG_COMP_OPT, master, AgentKeys.SERVICE_NAME, "HBASE",
-                        ARG_COMP_OPT, rs, AgentKeys.SERVICE_NAME, "HBASE",
-                        ARG_COMP_OPT, master, AgentKeys.APP_HOME, "/share/hbase/hbase-0.96.1-hadoop2",
-                        ARG_COMP_OPT, rs, AgentKeys.APP_HOME, "/share/hbase/hbase-0.96.1-hadoop2",
-                        ARG_COMP_OPT, AgentKeys.ROLE_NODE, SCRIPT_PATH, "agent/scripts/agent.py",
-                        ARG_RES_COMP_OPT, AgentKeys.ROLE_NODE, ResourceKeys.COMPONENT_PRIORITY, "1",
-                ],
-                true, false,
-                false)
-        def instanceD = launcher.service.loadPersistedClusterDescription(clustername)
-        dumpClusterDescription("$clustername:", instanceD)
-      def resource = instanceD.getResourceOperations()
+    def master = "hbase-master"
+    def rs = "hbase-rs"
+    ServiceLauncher<HoyaClient> launcher = buildAgentCluster(clustername,
+        [
+            (AgentKeys.ROLE_NODE): 5,
+            (master)             : 1,
+            (rs)                 : 5
+        ],
+        [
+            ARG_OPTION, CONTROLLER_URL, "http://localhost",
+            ARG_OPTION, PACKAGE_PATH, ".",
+            ARG_COMP_OPT, master, SCRIPT_PATH, "agent/scripts/agent.py",
+            ARG_COMP_OPT, rs, SCRIPT_PATH, "agent/scripts/agent.py",
+            ARG_RES_COMP_OPT, master, ResourceKeys.COMPONENT_PRIORITY, "2",
+            ARG_RES_COMP_OPT, rs, ResourceKeys.COMPONENT_PRIORITY, "3",
+            ARG_COMP_OPT, master, AgentKeys.SERVICE_NAME, "HBASE",
+            ARG_COMP_OPT, rs, AgentKeys.SERVICE_NAME, "HBASE",
+            ARG_COMP_OPT, master, AgentKeys.APP_HOME, "/share/hbase/hbase-0.96.1-hadoop2",
+            ARG_COMP_OPT, rs, AgentKeys.APP_HOME, "/share/hbase/hbase-0.96.1-hadoop2",
+            ARG_COMP_OPT, AgentKeys.ROLE_NODE, SCRIPT_PATH, "agent/scripts/agent.py",
+            ARG_RES_COMP_OPT, AgentKeys.ROLE_NODE, ResourceKeys.COMPONENT_PRIORITY, "1",
+        ],
+        true, false,
+        false)
+    def instanceD = launcher.service.loadPersistedClusterDescription(
+        clustername)
+    dumpClusterDescription("$clustername:", instanceD)
+    def resource = instanceD.getResourceOperations()
 
 
     def agentComponent = resource.getMandatoryComponent(AgentKeys.ROLE_NODE)
@@ -102,40 +102,41 @@ class TestBuildBasicAgent extends AgentTestBase {
     assert "2" == masterC.getMandatoryOption(ResourceKeys.COMPONENT_PRIORITY)
 
     def rscomponent = resource.getMandatoryComponent(rs)
-    assert "5" == rscomponent.getMandatoryOption(ResourceKeys.COMPONENT_INSTANCES)
+    assert "5" ==
+           rscomponent.getMandatoryOption(ResourceKeys.COMPONENT_INSTANCES)
 
-      // now create an instance with no role priority for the rs
-        try {
-            buildAgentCluster(clustername + "-2",
-                    [
-                            (AgentKeys.ROLE_NODE): 5,
-                            (master): 1,
-                            (rs): 5
-                    ],
-                    [
-                        ARG_RES_COMP_OPT, master, ResourceKeys.COMPONENT_PRIORITY, "2",
-                    ],
-                    true, false,
-                    false)
-            fail("Expected an exception")
-        } catch (BadConfigException expected) {
-        }
-        try {
-            buildAgentCluster(clustername + "-3",
-                    [
-                            (AgentKeys.ROLE_NODE): 5,
-                            (master): 1,
-                            (rs): 5
-                    ],
-                    [
-                        ARG_RES_COMP_OPT, master, ResourceKeys.COMPONENT_PRIORITY, "2",
-                        ARG_RES_COMP_OPT, rs, ResourceKeys.COMPONENT_PRIORITY, "2"],
-
-                    true, false,
-                    false)
-            fail("Expected an exception")
-        } catch (BadConfigException expected) {
-        }
+    // now create an instance with no role priority for the rs
+    try {
+      buildAgentCluster(clustername + "-2",
+          [
+              (AgentKeys.ROLE_NODE): 5,
+              (master)             : 1,
+              (rs)                 : 5
+          ],
+          [
+              ARG_RES_COMP_OPT, master, ResourceKeys.COMPONENT_PRIORITY, "2",
+          ],
+          true, false,
+          false)
+      fail("Expected an exception")
+    } catch (BadConfigException expected) {
     }
+    try {
+      buildAgentCluster(clustername + "-3",
+          [
+              (AgentKeys.ROLE_NODE): 5,
+              (master)             : 1,
+              (rs)                 : 5
+          ],
+          [
+              ARG_RES_COMP_OPT, master, ResourceKeys.COMPONENT_PRIORITY, "2",
+              ARG_RES_COMP_OPT, rs, ResourceKeys.COMPONENT_PRIORITY, "2"],
+
+          true, false,
+          false)
+      fail("Expected an exception")
+    } catch (BadConfigException expected) {
+    }
+  }
 
 }
