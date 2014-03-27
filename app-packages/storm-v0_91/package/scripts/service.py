e8e1eaa4da28be8f06d645bbf3c9b96e4048a541
Sumit Mohanty <smohanty@hortonworks.com>
2014-03-27 15:39:58 -0700

    Bug fixes to storm packaging

--- app-packages/storm-v0_91/package/scripts/service.py
+++ app-packages/storm-v0_91/package/scripts/service.py
@@ -33,20 +33,22 @@ def service(
   no_op_test = format("ls {pid_file} >/dev/null 2>&1 && ps `cat {pid_file}` >/dev/null 2>&1")
   jps_path = format("{java64_home}/bin/jps")
   grep_and_awk = "| grep -v grep | awk '{print $1}'"
-  pid_chk_cmd = format("{jps_path} -vl | grep \"backtype.storm.daemon.{name}\" {grep_and_awk}  > {pid_file}")
 
   if name == 'ui':
-    process_cmd = "^java.+backtype.storm.ui.core$"
+    #process_cmd = "^java.+backtype.storm.ui.core$"
+    pid_chk_cmd = format("{jps_path} -vl | grep \"^[0-9 ]*backtype.storm.ui.core\" {grep_and_awk}  > {pid_file}")
+  elif name == "rest_api":
+    process_cmd = format("{java64_home}/bin/java -jar {rest_lib_dir}/`ls {rest_lib_dir} | grep -wE storm-rest-[0-9.-]+\.jar` server")
+    crt_pid_cmd = format("pgrep -f \"{process_cmd}\" && pgrep -f \"{process_cmd}\" > {pid_file}")
   else:
-    process_cmd = format("^java.+backtype.storm.daemon.{name}$")
-
-  crt_pid_cmd = format("pgrep -f \"{process_cmd}\" && pgrep -f \"{process_cmd}\" > {pid_file}")
+    #process_cmd = format("^java.+backtype.storm.daemon.{name}$")
+    pid_chk_cmd = format("{jps_path} -vl | grep \"^[0-9 ]*backtype.storm.daemon.{name}\" {grep_and_awk}  > {pid_file}")
 
   if action == "start":
     if name == "rest_api":
       cmd = format("{process_cmd} {rest_api_conf_file} > {log_dir}/restapi.log")
     else:
-      cmd = format("env JAVA_HOME={java64_home} PATH=$PATH:{java64_home}/bin {storm_bin} {name}")
+      cmd = format("env JAVA_HOME={java64_home} PATH=$PATH:{java64_home}/bin STORM_BASE_DIR={app_root} STORM_CONF_DIR={conf_dir} {storm_bin} {name}")
 
     Execute(cmd,
             not_if=no_op_test,
@@ -55,22 +57,30 @@ def service(
             wait_for_finish=False
     )
 
-    content = None
-    for i in xrange(6):
-      Execute(pid_chk_cmd,
+    if name == "rest_api":
+      Execute(crt_pid_cmd,
               user=params.storm_user,
-              logoutput=True
+              logoutput=True,
+              tries=6,
+              try_sleep=10
       )
-      with open(pid_file) as f:
-        content = f.readline().strip()
-      if content.isdigit():
-        break;
-      File(pid_file, action = "delete")
-      time.sleep(10)
-      pass
+    else:
+      content = None
+      for i in xrange(6):
+        Execute(pid_chk_cmd,
+                user=params.storm_user,
+                logoutput=True
+        )
+        with open(pid_file) as f:
+          content = f.readline().strip()
+        if content.isdigit():
+          break;
+        File(pid_file, action = "delete")
+        time.sleep(10)
+        pass
 
-    if not content.isdigit():
-      raise Fail(format("Unable to start {name}"))
+      if not content.isdigit():
+        raise Fail(format("Unable to start {name}"))
 
   elif action == "stop":
     process_dont_exist = format("! ({no_op_test})")
